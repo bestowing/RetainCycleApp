9ca152516828363d58b692e00c22a85ef351ae34
bestowing <bestowing@naver.com>
2021-09-26 00:49:28 +0900

    chore: CustomView 문제 해결 주석에 에러 원인을 추가

--- RetainCycleApp/CustomView.swift
+++ RetainCycleApp/CustomView.swift
@@ -8,10 +8,11 @@
 
 import UIKit
 
-// 에러가 발생했음
-// class를 따르지 않는 프로토콜을 weak 변수로 선언해 사용할 때 발생하는 에러라고 함
-// https://jusung.github.io/classOnlyProtocol/를 참고하여 프로토콜이 class를 따르도록 선언함
-// Using 'class' keyword for protocol inheritance is deprecated; use 'AnyObject' instead
+// CustomView에서 delegate로 RedViewController의 인스턴스를 참조하고 있음 -> VC와 CustomView간에 strong reference cycle이 형성됨
+// delegate를 weak로 선언 -> 에러가 발생했음
+    // class를 따르지 않는 프로토콜을 weak 변수로 선언해 사용할 때 발생하는 에러라고 함
+    // https://jusung.github.io/classOnlyProtocol/를 참고하여 프로토콜이 class를 따르도록 선언함
+    // 그러나 class 키워드가 deprecated 되었다고 함 -> AnyObject를 사용
 // 더 이상 strong reference cycle이 발생하지 않음
 protocol  CustomDelegate: AnyObject {
     func foo(_ value: Int)
