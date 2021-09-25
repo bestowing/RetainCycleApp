50837ab265ccad16ad44212408268fbf18027fca
bestowing <bestowing@naver.com>
2021-09-26 00:42:00 +0900

    fix: case 2 해결

--- RetainCycleApp/CustomView.swift
+++ RetainCycleApp/CustomView.swift
@@ -8,7 +8,12 @@
 
 import UIKit
 
-protocol  CustomDelegate {
+// 에러가 발생했음
+// class를 따르지 않는 프로토콜을 weak 변수로 선언해 사용할 때 발생하는 에러라고 함
+// https://jusung.github.io/classOnlyProtocol/를 참고하여 프로토콜이 class를 따르도록 선언함
+// Using 'class' keyword for protocol inheritance is deprecated; use 'AnyObject' instead
+// 더 이상 strong memory cycle이 발생하지 않음
+protocol  CustomDelegate: AnyObject {
     func foo(_ value: Int)
 }
 
@@ -21,7 +26,7 @@ class CustomView: UIView {
         }
     }
     
-    var delegate : CustomDelegate? = nil
+    weak var delegate : CustomDelegate? = nil
 
     override func layoutSubviews() {
         super.layoutSubviews()
